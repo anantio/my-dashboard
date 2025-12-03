e45c880c273beb0c32f59b93a30fbf5af93e47f1
anantio <ananttomer.4@gmail.com>
2025-12-03 12:21:40 +0530

    Flatten project structure and remove nested folder

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import DashboardFeatureBlock from "./Components/feature/DashboardFeatureBlock";
+
+function App() {
+  return (
+    <div className="App">
+      <DashboardFeatureBlock />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/feature/DashboardFeatureBlock.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const DashboardFeatureBlock = () => {
+  return <div>DashboardFeatureBlock</div>;
+};
+
+export default DashboardFeatureBlock;
